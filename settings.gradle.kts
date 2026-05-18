62d6bf637b9e8ddc56c99d4d8af2bd4625204681
denotas235 <recebeumoneyantonio@gmail.com>
2026-05-18 19:19:37 +0200

    Adicionado settings.gradle.kts com configuração de plugins

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("net.fabricmc.fabric-loom-remap") version "1.16-SNAPSHOT"
+    }
+}
+rootProject.name = "NexusOptimizer"
